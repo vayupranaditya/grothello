597287e2d0776a0307701ff9408dba6a2de03237
vayupranaditya <bagusvayuprana@gmail.com>
2019-04-25 02:24:53 +0700

    now you can choose from available moves

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 $('document').ready(() => {
 	initGrid();
+	safeSet(4, 4, 0);
+	safeSet(5, 5, 0);
+	safeSet(4, 5, 1);
+	safeSet(5, 4, 1);
 	let turn = 0;
+	getAvailableMove(turn%2);
 	$('.is-1').click((e) => {
 		let html = e.target.innerHTML;
 		let pos = getLocation(e.target.id);
 		if (setContent(pos.x, pos.y, turn % 2)) {
 			turn++;
+			setAllAttribute('available', false);
 		}
+		getAvailableMove(turn%2);
 	});
 });
--- funcs.js
+++ funcs.js
@@ -1,22 +1,48 @@
 const getLocation = (id) => {
 	let x = id.substr(3, 3);
 	let y = id.substr(1, 1);
-	console.log(x+','+y);
 	return {'x' : x, 'y' : y};
 };
+
+const setAllAttribute = (key, value) => {
+	for (let i=1; i <= 8; i++) {
+		for (let j=1; j <= 8; j++) {
+			let id = getId(i, j);
+			$(id).attr('data-'+key, value);
+		}
+	}
+}
 
 const getContent = (x, y) => {
 	return getAttribute(x, y, 'content');
 };
 
 const setContent = (x, y, obj) => {
+	// console.log(getAttribute(x, y, 'immutable') == true);
+	console.log(getAttribute(x, y, 'available') != 'true');
+	if (getAttribute(x, y, 'available') != 'true') {
+		console.log('NO!');
+		return false;
+	}
 	setAttribute(x, y, 'content', obj);
 	setAttribute(x, y, 'immutable', true);
 	if (obj === 0) {
-		return fill(x, y, 'black');
+		fill(x, y, 'black');
 	} else {
-		return fill(x, y, 'white');
+		fill(x, y, 'white');
 	}
+	return true;
+};
+
+const safeSet = (x, y, obj) => {
+	setAttribute(x, y, 'content', obj);
+	setAttribute(x, y, 'immutable', true);
+	if (obj === 0) {
+		fill(x, y, 'black');
+	} else {
+		fill(x, y, 'white');
+	}
+	return true;
 };
 
 const setAttribute = (x, y, key, value) => {
@@ -29,6 +55,11 @@ const getAttribute = (x, y, key) => {
 	return $(id).attr('data-'+key);
 }
 
+const removeAttribute = (x, y, key) => {
+	let id = getId(x, y);
+	$(id).removeAttr('data-'+key);
+}
+
 const getObjLocations = (obj) => {
 	let locations = [];
 	for (let i = 1; i <= 8; i++) {
@@ -38,7 +69,6 @@ const getObjLocations = (obj) => {
 			}
 		}		
 	}
-	console.log(locations)
 	return locations;
 }
 
@@ -49,20 +79,14 @@ const getFreeSurroundings = (poss) => {
 		x = poss[i].x;
 		y = poss[i].y;
 		if (y < n && getContent(x, y+1) === '-') surs.push({'x' : x, 'y' : y+1});
-		if (y > 0 && getContent(x, y-1) === '-') surs.push({'x' : x, 'y' : y-1});
+		if (y > 1 && getContent(x, y-1) === '-') surs.push({'x' : x, 'y' : y-1});
 		if (x < n && getContent(x+1, y) === '-') surs.push({'x' : x+1, 'y' : y});
-		if (x > 0 && getContent(x-1, y) === '-') surs.push({'x' : x-1, 'y' : y});
+		if (x > 1 && getContent(x-1, y) === '-') surs.push({'x' : x-1, 'y' : y});
 		if (x < n && y < n && getContent(x+1, y+1) === '-') surs.push({'x' : x+1, 'y' : y+1});
-		if (x > 0 && y > 0 && getContent(x-1, y-1) === '-') surs.push({'x' : x-1, 'y' : y-1});
-		if (x < n && y > 0 && getContent(x+1, y-1) === '-') surs.push({'x' : x+1, 'y' : y-1});
-		if (x > 0 && y < n && getContent(x-1, y+1) === '-') surs.push({'x' : x-1, 'y' : y+1});
+		if (x > 1 && y > 1 && getContent(x-1, y-1) === '-') surs.push({'x' : x-1, 'y' : y-1});
+		if (x < n && y > 1 && getContent(x+1, y-1) === '-') surs.push({'x' : x+1, 'y' : y-1});
+		if (x > 1 && y < n && getContent(x-1, y+1) === '-') surs.push({'x' : x-1, 'y' : y+1});
 	}
-	let id;
-	for (let i = 0; i < surs.length; i++) {
-		id = getId(surs[i].x, surs[i].y)
-		$(id).html('<div style="width: 100%; height: 100%; border-radius: 50%; background-color: #0004"></div>');
-	}
-	console.log(surs);
 	return surs;
 }
 
@@ -75,16 +99,158 @@ const initGrid = () => {
 	for (let i = 1; i <= 8; i++) {
 		for (let j = 1; j <= 8 ; j++) {
 			setAttribute(i, j, 'content', '-');
+			setAttribute(i, j, 'immutable', false);
+			removeAttribute(i, j, 'available');
+			setAttribute(i, j, 'available', false);
 		}
 	}
 }
 
 const fill = (x, y, color) => {
+	removeFill(x, y);
 	let id  = getId(x, y);
-	if ($(id).attr('data-immutable') === "true") {
-		$(id).html('<div style="width: 100%; height: 100%; border-radius: 50%; background-color: '+color+'"></div>');
-		return true;
-	} else {
-		return false;
+	$(id).html('<div style="width: 100%; height: 100%; border-radius: 50%; background-color: '+color+'"></div>');
+}
+
+const removeFill = (x, y, color) => {
+	let id  = getId(x, y);
+	$(id).html('');
+}
+
+const getN = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (y < n && getContent(x, y+1) == obj) {
+		y++;
+		i++;
+	}
+	y++;
+	if (getContent(x, y++) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getS = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (y > 1 && getContent(x, y-1) == obj) {
+		y--;
+		i++;
+	}
+	y--;
+	if (getContent(x, y--) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getE = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (x < n && getContent(x+1, y) == obj) {
+		x++;
+		i++;
+	}
+	x++;
+	if (getContent(x++, y) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getW = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (x > 1 && getContent(x-1, y) == obj) {
+		x--;
+		i++;
+	}
+	x--;
+	if (getContent(x--, y) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getNE = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (x < n && y < n && getContent(x+1, y+1) == obj) {
+		x++;
+		y++
+		i++;
+	}
+	x++;
+	y++;
+	if (getContent(x++, y++) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getSE = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (x < n && y > 1 && getContent(x+1, y-1) == obj) {
+		x++;
+		y--;
+		i++;
+	}
+	x++;
+	y--;
+	if (getContent(x++, y--) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getSW = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (x > 1 && y > 1 && getContent(x-1, y-1) == obj) {
+		x--;
+		y--;
+		i++;
+	}
+	x--;
+	y--;
+	if (getContent(x--, y--) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getNW = (x, y, obj) => {
+	let i = 0;
+	n = 8;
+	while (x > 1 && y < n && getContent(x-1, y+1) == obj) {
+		x--;
+		y++;
+		i++;
+	}
+	x--;
+	y++;
+	if (getContent(x--, y++) == (obj+1)%2) return i;
+	return 0;
+}
+
+const getAvailableMove = (obj) => {
+	obj = (obj+1) % 2;
+	availableMoves = [];
+	locs = getObjLocations(obj);
+	surs = getFreeSurroundings(locs);
+	let num;
+	for (let i = 0; i < surs.length; i++) {
+		x = surs[i].x;
+		y = surs[i].y;
+		num = 0;
+		num += getN(x, y, obj);
+		num += getE(x, y, obj);
+		num += getS(x, y, obj);
+		num += getW(x, y, obj);
+		num += getNE(x, y, obj);
+		num += getSE(x, y, obj);
+		num += getSW(x, y, obj);
+		num += getNW(x, y, obj);
+		if (num > 0) {
+			availableMoves.push(surs[i]);
+			removeAttribute(x, y, 'available');
+			setAttribute(x, y, 'available', true);
+		} else {
+			removeFill(x, y);
+		}
+	}
+	let id;
+	for (let i = 0; i < availableMoves.length; i++) {
+		x = availableMoves[i].x;
+		y = availableMoves[i].y;
+		fill(x, y, '#bbb');
 	}
 }
